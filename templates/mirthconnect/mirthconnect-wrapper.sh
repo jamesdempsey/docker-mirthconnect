fda7296b224f1353559da3246872537cc972a2e8
James Dempsey <james.e.dempsey@gmail.com>
2015-02-24 23:15:50 -0500

    Reconfigure internal ports and logging for Mirth Connect install

--- templates/mirthconnect/mirthconnect-wrapper.sh
+++ templates/mirthconnect/mirthconnect-wrapper.sh
@@ -3,4 +3,4 @@
 ./mcservice start
 while ! echo exit | nc localhost 443; do sleep 0.1; done
 java -jar mirth-cli-launcher.jar -a https://localhost:443 -u admin -p admin -v 0.0.0 -s mirthconnect-cli.txt
-tail -f /usr/local/mirthconnect/logs/mirth.log
+tail -f /opt/mirthconnect/logs/mirth.log

9b4dd1a4fc401fb2b6829d2511854b44b4b77575
Alex Rothberg <agrothberg@gmail.com>
2015-04-06 15:15:44 -0400

    Make sure the service is actually running. Kill tail if the service dies

--- templates/mirthconnect/mirthconnect-wrapper.sh
+++ templates/mirthconnect/mirthconnect-wrapper.sh
@@ -3,12 +3,32 @@
 # Start NGiNX reverse proxy
 /usr/sbin/nginx
 
+# Clean-up old log file
+rm -f /opt/mirthconnect/logs/mirth.log
+
 # Start Mirth Connect
 ./mcservice start
+
+# Make sure that the service has actually started
+while [ ! -f /opt/mirthconnect/logs/mirth.log ]
+do
+  sleep 0.1
+done
+
+# Don't spin wait if the service is dead
 ./mcservice status
+if [[ `./mcservice status` != "The daemon is running." ]]; then
+    echo "The daemon is not running"
+    exit 1
+fi
+
 while ! echo exit | nc localhost 443; do sleep 0.1; done
+
 pids=$(pgrep java)
 echo $pids
+
 java -jar mirth-cli-launcher.jar -a https://localhost:443 -u admin -p admin -v 0.0.0 -s mirthconnect-cli.txt
-tail -f /opt/mirthconnect/logs/mirth.log
+
+tail -f /opt/mirthconnect/logs/mirth.log --pid=$pids
+
 service nginx stop
